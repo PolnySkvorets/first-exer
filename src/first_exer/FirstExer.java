6ab2c6d655d26eedf261e0e6efecb4a5d0a0c7f2
Denis <tapfereherz@gmail.com>
2018-09-12 20:24:56 +0700

    урок - кнопка выхода

--- src/first_exer/FirstExer.java
+++ src/first_exer/FirstExer.java
@@ -1,11 +1,13 @@
 package first_exer;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 
-public class FirstExer extends JFrame {// наследуем от класса jFrame , то есть создаем рамку
+public class FirstExer extends JFrame {// наследуем от класса jFrame (контейнер верхнего уровня) , то есть создаем рамку
 
-    public  FirstExer () { /** конструктор окна */
+    public  FirstExer () { /** конструктор окна  */
 
     initUI();
 
@@ -13,15 +15,40 @@ public class FirstExer extends JFrame {// наследуем от класса j
 
     private void initUI() {
 
-        setTitle("First Exercise");  // параметры окна - подпись, размеры, привязка, выход по зпкрытию
+        JButton quitButton = new JButton("Quit"); // создание объекты кнопки выхода
+
+        quitButton.addActionListener((ActionEvent event) -> { // задание дейстаия при нажатии на кнопку
+            System.exit(0);
+        });
+            createLayout(quitButton);
+
+
+        setTitle("First Exercise with quit button");  // параметры окна - подпись, размеры, привязка, выход по зпкрытию
         setSize(300, 200);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    private void createLayout(JComponent... arg) {
+
+      Container pane = getContentPane();
+      GroupLayout gl = new GroupLayout(pane);
+      pane.setLayout(gl);
+
+      gl.setAutoCreateContainerGaps(true);
+
+      gl.setHorizontalGroup(gl.createSequentialGroup().addComponent(arg[0]));
+
+      gl.setVerticalGroup(gl.createSequentialGroup().addComponent(arg[0]));
+
+
+    }
+
 
     public static void main(String[] args) {
-	    FirstExer ex = new FirstExer();
-	    ex.setVisible(true);// вывод окна
+        EventQueue.invokeLater(() -> {
+            FirstExer ex = new FirstExer();
+            ex.setVisible(true);// вывод окна
+        });
     }
 }
